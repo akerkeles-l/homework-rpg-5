f097738550942ec463575705d37c0c13690797bb
akerkeles-l <akerke.les@narxoz.kz>
2026-03-13 20:30:36 +0500

    done with fire rune decorator file

--- src/com/narxoz/rpg/decorator/FireRuneDecorator.java
+++ src/com/narxoz/rpg/decorator/FireRuneDecorator.java
@@ -1,25 +1,24 @@
 package com.narxoz.rpg.decorator;
 
 public class FireRuneDecorator extends ActionDecorator {
+    
     public FireRuneDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        // TODO: Decide how this decorator changes the visible action name.
-        return super.getActionName();
+        return "🔥 " + getWrappedAction().getActionName() + " with Fire Rune";
     }
 
     @Override
     public int getDamage() {
-        // TODO: Add fire-related behavior on top of wrapped damage.
-        return super.getDamage();
+        // Fire adds 8 bonus damage
+        return getWrappedAction().getDamage() + 8;
     }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Append or compose the fire effect description.
-        return super.getEffectSummary();
+        return getWrappedAction().getEffectSummary() + " + burning (8 fire damage)";
     }
 }
