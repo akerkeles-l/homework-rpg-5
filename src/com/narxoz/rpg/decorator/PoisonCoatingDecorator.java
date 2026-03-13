f1d941d3989c6c98f025b39187dcdb4da104886d
akerkeles-l <akerke.les@narxoz.kz>
2026-03-13 20:32:07 +0500

    done with poison coating decorator file

--- src/com/narxoz/rpg/decorator/PoisonCoatingDecorator.java
+++ src/com/narxoz/rpg/decorator/PoisonCoatingDecorator.java
@@ -1,25 +1,24 @@
 package com.narxoz.rpg.decorator;
 
 public class PoisonCoatingDecorator extends ActionDecorator {
+    
     public PoisonCoatingDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        // TODO: Decide how this decorator changes the visible action name.
-        return super.getActionName();
+        return "☠️ " + getWrappedAction().getActionName() + " with Poison Coating";
     }
 
     @Override
     public int getDamage() {
-        // TODO: Add poison-related behavior on top of wrapped damage.
-        return super.getDamage();
+        // Poison adds 5 bonus damage over time
+        return getWrappedAction().getDamage() + 5;
     }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Append or compose the poison effect description.
-        return super.getEffectSummary();
+        return getWrappedAction().getEffectSummary() + " + poison (5 damage over time)";
     }
 }
